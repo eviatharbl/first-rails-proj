23ed71bfcd2bc29f6122e91e17af31c208162337
Eviathar <eviatharbl@gmail.com>
2012-07-25 00:26:36 +0300

    simple cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FirstRailsProj::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
+
   resources :products
 
   get "say/hello"
@@ -54,7 +60,7 @@ FirstRailsProj::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'store#index'
 
   # See how all your routes lay out with "rake routes"
 
